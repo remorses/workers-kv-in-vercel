4267425e98ebd741e106f9bd6c2f7dbb976be2d1
remorses <beats.by.morse@gmail.com>
2023-01-26 15:14:58 +0100

    innit

---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+import KvStorage from 'cloudflare-kv-storage-rest'
+
+const { ACCOUNTID, KEY } = process.env
+
+const store = new KvStorage({
+    namespace: 'namespace',
+    accountId: 'accountId',
+    fetch,
+    FormData,
+})
+
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+    console.time('kv')
+    const res = await store.get('xxx')
+    console.log(res)
+    console.timeEnd('kv')
+    await store.put('xxx', 'test')
+    return NextResponse.next()
+}
